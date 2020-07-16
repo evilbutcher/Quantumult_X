1a0316770b27a33959a35f66ecca58e22958574f
evilbutcher <evilbutcher@users.noreply.github.com>
2020-07-16 12:05:08 +0800

    Update glados.js

--- check_in/glados/glados.js
+++ check_in/glados/glados.js
@@ -3,10 +3,10 @@
 登陆链接：https://glados.rocks/，登陆即可获取Cookie。
 
 [rewrite_local]
-https:\/\/glados\.rocks\/api\/user\/status url script-request-header Local/check_in/glados/glados.js
+https:\/\/glados\.rocks\/api\/user\/status url script-request-header https://github.com/evilbutcher/Quantumult_X/raw/master/check_in/glados/glados.js
 
 [task_local]
-1 0 * * * Local/check_in/glados/glados.js
+1 0 * * * https://github.com/evilbutcher/Quantumult_X/raw/master/check_in/glados/glados.js
 
 hostname = glados.rocks
 */
