fbec0036573dec8e496bc969f12fa6e0998845ad
evilbutcher <evilbutcher@users.noreply.github.com>
2020-07-16 18:11:01 +0800

    update tools

--- tools/API from Peng-YM.js
+++ tools/API from Peng-YM.js
@@ -1,5 +1,5 @@
 function API(name = "untitled", debug = false) {
-  return new class {
+  return new (class {
     constructor(name, debug) {
       this.name = name;
       this.debug = debug;
@@ -18,7 +18,7 @@ function API(name = "untitled", debug = false) {
 
           return {
             request,
-            fs,
+            fs
           };
         } else {
           return null;
@@ -27,12 +27,12 @@ function API(name = "untitled", debug = false) {
       this.initCache();
 
       const delay = (t, v) =>
-        new Promise(function (resolve) {
+        new Promise(function(resolve) {
           setTimeout(resolve.bind(null, v), t);
         });
 
-      Promise.prototype.delay = function (t) {
-        return this.then(function (v) {
+      Promise.prototype.delay = function(t) {
+        return this.then(function(v) {
           return delay(t, v);
         });
       };
@@ -49,7 +49,12 @@ function API(name = "untitled", debug = false) {
           if (this.isLoon || this.isSurge)
             $httpClient.get(options, (err, response, body) => {
               if (err) reject(err);
-              else resolve({ status: response.status, headers: response.headers, body });
+              else
+                resolve({
+                  status: response.status,
+                  headers: response.headers,
+                  body
+                });
             });
           else
             this.node.request(options, (err, response, body) => {
@@ -70,7 +75,12 @@ function API(name = "untitled", debug = false) {
           if (this.isLoon || this.isSurge) {
             $httpClient.post(options, (err, response, body) => {
               if (err) reject(err);
-              else resolve({ status: response.status, headers: response.headers, body });
+              else
+                resolve({
+                  status: response.status,
+                  headers: response.headers,
+                  body
+                });
             });
           } else {
             this.node.request.post(options, (err, response, body) => {
@@ -86,7 +96,8 @@ function API(name = "untitled", debug = false) {
 
     // initialize cache
     initCache() {
-      if (this.isQX) this.cache = JSON.parse($prefs.valueForKey(this.name) || "{}");
+      if (this.isQX)
+        this.cache = JSON.parse($prefs.valueForKey(this.name) || "{}");
       if (this.isLoon || this.isSurge)
         this.cache = JSON.parse($persistentStore.read(this.name) || "{}");
 
@@ -98,7 +109,7 @@ function API(name = "untitled", debug = false) {
             fpath,
             JSON.stringify({}),
             { flag: "wx" },
-            (err) => console.log(err)
+            err => console.log(err)
           );
         }
         this.root = {};
@@ -110,11 +121,13 @@ function API(name = "untitled", debug = false) {
             fpath,
             JSON.stringify({}),
             { flag: "wx" },
-            (err) => console.log(err)
+            err => console.log(err)
           );
           this.cache = {};
         } else {
-          this.cache = JSON.parse(this.node.fs.readFileSync(`${this.name}.json`));
+          this.cache = JSON.parse(
+            this.node.fs.readFileSync(`${this.name}.json`)
+          );
         }
       }
     }
@@ -129,21 +142,21 @@ function API(name = "untitled", debug = false) {
           `${this.name}.json`,
           data,
           { flag: "w" },
-          (err) => console.log(err)
+          err => console.log(err)
         );
         this.node.fs.writeFileSync(
           "root.json",
           JSON.stringify(this.root),
           { flag: "w" },
-          (err) => console.log(err)
-        )
+          err => console.log(err)
+        );
       }
     }
 
     write(data, key) {
       this.log(`SET ${key}`);
-      if (key.indexOf('#') !== -1) {
-        key = key.substr(1)
+      if (key.indexOf("#") !== -1) {
+        key = key.substr(1);
         if (this.isSurge & this.isLoon) {
           $persistentStore.write(data, key);
         }
@@ -161,8 +174,8 @@ function API(name = "untitled", debug = false) {
 
     read(key) {
       this.log(`READ ${key}`);
-      if (key.indexOf('#') !== -1) {
-        key = key.substr(1)
+      if (key.indexOf("#") !== -1) {
+        key = key.substr(1);
         if (this.isSurge & this.isLoon) {
           $persistentStore.read(data, key);
         }
@@ -180,8 +193,8 @@ function API(name = "untitled", debug = false) {
     delete(key) {
       this.log(`DELETE ${key}`);
       delete this.cache[key];
-      if (key.indexOf('#') !== -1) {
-        key = key.substr(1)
+      if (key.indexOf("#") !== -1) {
+        key = key.substr(1);
         if (this.isSurge & this.isLoon) {
           $persistentStore.write(null, key);
         }
@@ -199,10 +212,13 @@ function API(name = "untitled", debug = false) {
 
     // notification
     notify(title, subtitle = "", content = "", options = {}) {
-      const openURL = options['open-url'];
-      const mediaURL = options['media-url'];
+      const openURL = options["open-url"];
+      const mediaURL = options["media-url"];
 
-      const content_ = content + (openURL ? `\n点击跳转: ${openURL}` : "") + (mediaURL ? `\n多媒体: ${mediaURL}` : "");
+      const content_ =
+        content +
+        (openURL ? `\n点击跳转: ${openURL}` : "") +
+        (mediaURL ? `\n多媒体: ${mediaURL}` : "");
 
       if (this.isQX) $notify(title, subtitle, content, options);
       if (this.isSurge) $notification.post(title, subtitle, content_);
@@ -212,7 +228,7 @@ function API(name = "untitled", debug = false) {
           const push = require("push");
           push.schedule({
             title: title,
-            body: (subtitle ? subtitle + "\n" : "") + content_,
+            body: (subtitle ? subtitle + "\n" : "") + content_
           });
         } else {
           console.log(`${title}\n${subtitle}\n${content_}\n\n`);
@@ -234,19 +250,19 @@ function API(name = "untitled", debug = false) {
     }
 
     wait(millisec) {
-      return new Promise((resolve) => setTimeout(resolve, millisec));
+      return new Promise(resolve => setTimeout(resolve, millisec));
     }
 
     done(value = {}) {
       if (this.isQX || this.isLoon || this.isSurge) {
         $done(value);
       } else if (this.isNode && !this.isJSBox) {
-        if (typeof $context !== 'undefined') {
+        if (typeof $context !== "undefined") {
           $context.headers = value.headers;
           $context.statusCode = value.statusCode;
           $context.body = value.body;
         }
       }
     }
-  }(name, debug);
+  })(name, debug);
 }
