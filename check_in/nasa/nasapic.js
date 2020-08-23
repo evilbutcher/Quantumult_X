55c1243b2d6b087c8584e65db79a1f4654dbf413
evilbutcher <pgcrfhht@outlook.com>
2020-08-23 15:36:29 +0800

    增加【NASA每日一图】

--- check_in/nasa/nasapic.js
+++ check_in/nasa/nasapic.js
@@ -1,3 +1,44 @@
+/*
+【NASA每日一图】@evilbutcher
+
+【仓库地址】https://github.com/evilbutcher/Quantumult_X/tree/master（欢迎star🌟）
+
+【BoxJs】https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/evilbutcher.boxjs.json
+
+【致谢】
+感谢Peng-YM的OpenAPI.js！
+
+⚠️【免责声明】
+------------------------------------------
+1、此脚本仅用于学习研究，不保证其合法性、准确性、有效性，请根据情况自行判断，本人对此不承担任何保证责任。
+2、由于此脚本仅用于学习研究，您必须在下载后 24 小时内将所有内容从您的计算机或手机或任何存储设备中完全删除，若违反规定引起任何事件本人对此均不负责。
+3、请勿将此脚本用于任何商业或非法目的，若违反规定请自行对此负责。
+4、此脚本涉及应用与本人无关，本人对因此引起的任何隐私泄漏或其他后果不承担任何责任。
+5、本人对任何脚本引发的问题概不负责，包括但不限于由脚本错误引起的任何损失和损害。
+6、如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
+7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
+
+【Surge】
+-----------------
+[Script]
+NASA每日一图 = type=cron,cronexp=5 0 * * *,script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/nasa/nasapic.js
+
+【Loon】
+-----------------
+[Script]
+cron "5 0 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/nasa/nasapic.js, tag=NASA每日一图
+
+【Quantumult X】
+-----------------
+[task_local]
+5 0 * * * https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/nasa/nasapic.js, tag=NASA每日一图
+
+【icon】
+透明：https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/picture/nasa_tran.png
+彩色：https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/picture/nasa.png
+*/
+
+
 const $ = new API("NASA");
 const ERR = MYERR();
 const translate = JSON.parse($.read("translate")) || "false";
@@ -82,7 +123,7 @@ function showmsg() {
     detail = `©️Copyright：${copyright}\n⌚️Date：${time}\n${exp}`;
   }
   var cover = $.data.url;
-  $.notify("NASA每日一图", title, detail, { "media-url": cover });
+  $.notify("NASA每日一图", title, detail, { "media-url": cover, "open-url":cover });
 }
 
 function MYERR() {
